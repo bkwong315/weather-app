8c3d4bb9593f37986151402b1d92ed4bff6ee0fe
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-08 15:08:46 -0800

    Add input html elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss';
 
 function App() {
-
   return (
     <div className="App">
-      
+      <div className="input-container">
+        <label htmlFor="city">City:</label>
+        <input type="text" name="city" id="city" />
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
