cc37737e1147e578a807c8fbad7dfaaffb82fa10
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-09 22:01:49 -0800

    Refactor api call code

--- src/App.tsx
+++ src/App.tsx
@@ -8,68 +8,97 @@ function App() {
   const [loading, setLoading] = useState<boolean>(true);
   const [selectedDate, setSelectedDate] = useState<SelectedDateTemplate>();
 
+  const units = 'imperial';
+  const API_KEY = 'a8d2cbc847ed40ce311d65394e47f905';
+
   useEffect(() => {
     const cityInput = document.querySelector('#city') as HTMLInputElement;
     const submitBtn = document.querySelector('#city ~ button');
-    const units = 'imperial';
-    const API_KEY = 'a8d2cbc847ed40ce311d65394e47f905';
+
+    submitBtn?.addEventListener('click', submitQuery);
 
     async function submitQuery() {
       const query = cityInput.value;
-      const geocode = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${API_KEY}`);
-      const geocodeData = await geocode.json();
-      const weather = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${geocodeData[0].lat}&lon=${geocodeData[0].lon}&units=${units}&appid=${API_KEY}`,
-      );
-      const weatherData = await weather.json();
-
-      const filteredList = weatherData.list.reduce(
-        (weatherTimestamps: { dt_txt: string }[], currentTimestamp: { dt_txt: string }) => {
-          const newDate = weatherTimestamps.every(
-            (timestamp) => timestamp.dt_txt.split(' ')[0] !== currentTimestamp.dt_txt.split(' ')[0],
-          );
-
-          if (newDate) weatherTimestamps.push(currentTimestamp);
-
-          return weatherTimestamps;
-        },
-        [],
-      );
-
-      setLoading(false);
-
-      try {
-        await setSelectedDate({
-          iconURL: `./src/imgs/weather-icons/${filteredList[0].weather[0].icon}.svg`,
-          description: filteredList[0].weather[0].description,
-          city: weatherData.city.name,
-          country: weatherData.city.country,
-          temp: Math.floor(filteredList[0].main.temp).toString(),
-          feelsLike: Math.floor(filteredList[0].main.feels_like).toString(),
-          tempMin: Math.floor(filteredList[0].main.temp_min).toString(),
-          tempMax: Math.floor(filteredList[0].main.temp_max).toString(),
-          tempUnit: 'F',
-          precipitation: filteredList[0].pop,
-          humidity: filteredList[0].main.humidity,
-          wind: Math.floor(filteredList[0].wind.speed).toString(),
-          windUnit: 'mph',
-          windDeg: filteredList[0].wind.deg,
-        });
-      } catch (err) {
-        console.error(err);
-      }
+      sendQuery(query);
     }
 
-    submitBtn?.addEventListener('click', submitQuery);
+    sendQuery('london');
 
     return () => submitBtn?.removeEventListener('click', submitQuery);
   }, []);
 
+  async function sendQuery(query: string) {
+    const geocodeData = await fetchData(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${API_KEY}`,
+    );
+    const weatherData = await fetchData(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${geocodeData[0].lat}&lon=${geocodeData[0].lon}&units=${units}&appid=${API_KEY}`,
+    );
+
+    updateSelectedDateData(weatherData);
+  }
+
+  async function fetchData(apiURL: string) {
+    let response;
+    try {
+      response = await fetch(apiURL);
+    } catch (err) {
+      console.log(err);
+    }
+
+    return await response?.json();
+  }
+
+  function getFiveDayForecast(weatherData: { list: []; city: object }) {
+    const reducedList = weatherData.list.reduce(
+      (weatherTimestamps: { dt_txt: string }[], currentTimestamp: { dt_txt: string }) => {
+        const newDate = weatherTimestamps.every(
+          (timestamp) => timestamp.dt_txt.split(' ')[0] !== currentTimestamp.dt_txt.split(' ')[0],
+        );
+
+        if (newDate) weatherTimestamps.push(currentTimestamp);
+
+        return weatherTimestamps;
+      },
+      [],
+    );
+
+    return reducedList;
+  }
+
+  function updateSelectedDateData(weatherData: { list: []; city: { name: string; country: string } }) {
+    // @ts-expect-error : Not sure how to fix error
+    const forecast: Array<{
+      weather: Array<{ description: string; icon: string }>;
+      main: { temp: string; feels_like: string; temp_min: string; temp_max: string; humidity: string };
+      pop: string;
+      wind: { speed: string; deg: string };
+    }> = getFiveDayForecast(weatherData);
+
+    setSelectedDate({
+      iconURL: `./src/imgs/weather-icons/${forecast[0].weather[0].icon}.svg`,
+      description: forecast[0].weather[0].description,
+      city: weatherData.city.name,
+      country: weatherData.city.country,
+      temp: Math.floor(parseFloat(forecast[0].main.temp)).toString(),
+      feelsLike: Math.floor(parseFloat(forecast[0].main.feels_like)).toString(),
+      tempMin: Math.floor(parseFloat(forecast[0].main.temp_min)).toString(),
+      tempMax: Math.floor(parseFloat(forecast[0].main.temp_max)).toString(),
+      tempUnit: 'F',
+      precipitation: forecast[0].pop,
+      humidity: forecast[0].main.humidity,
+      wind: (Math.floor(parseFloat(forecast[0].wind.speed) * 10) / 10).toString(),
+      windUnit: 'mph',
+      windDeg: forecast[0].wind.deg,
+    });
+
+    setLoading(false);
+  }
+
   return (
     <div className="App">
       <div className="input-container">
-        <label htmlFor="city">City:</label>
-        <input type="text" name="city" id="city" />
+        <input type="text" name="city" id="city" placeholder="Enter City" />
         <button>Submit</button>
       </div>
       {!loading && <SelectedDateDisplay info={selectedDate as SelectedDateTemplate} />}
