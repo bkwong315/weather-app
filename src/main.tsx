393754c87eb29055975cdd9a74460c8abe823561
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-09 20:18:50 -0800

    Add attribution for icons

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,19 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
     <div className="attribution">
-      Photo by{' '}
-      <a href="https://unsplash.com/@mischievous_penguins?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-        Casey Horner
-      </a>{' '}
-      on{' '}
-      <a href="https://unsplash.com/photos/wKjIeK4QSnk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-        Unsplash
-      </a>
+      <span className="unsplash">
+        Photo by{' '}
+        <a href="https://unsplash.com/@mischievous_penguins?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Casey Horner
+        </a>{' '}
+        on{' '}
+        <a href="https://unsplash.com/photos/wKjIeK4QSnk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+          Unsplash
+        </a>
+      </span>
+      <span className="vecteezy">
+        <a href="https://www.vecteezy.com/free-vector/weather-icon-set">Weather Icon Set Vectors by Vecteezy</a>
+      </span>
     </div>
   </React.StrictMode>,
 );
