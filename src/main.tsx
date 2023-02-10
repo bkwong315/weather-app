edf840f940d2feab68271bb8a0029d5916e84e9a
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-02-09 18:26:02 -0800

    Add bg img and Montserrat

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <div className="attribution">
+      Photo by{' '}
+      <a href="https://unsplash.com/@mischievous_penguins?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Casey Horner
+      </a>{' '}
+      on{' '}
+      <a href="https://unsplash.com/photos/wKjIeK4QSnk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Unsplash
+      </a>
+    </div>
   </React.StrictMode>,
-)
+);
